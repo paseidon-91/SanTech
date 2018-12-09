651e5dbc38a64a3982c730e8cc5399bdf96d33a4
paseidon-91 <alex.pyshkin.91@gmail.com>
2018-12-10 00:05:11 +0300

    Переписаны стили шаблона. Корректная DOM-модель.

--- src/main/java/santech/controller/UserController.java
+++ src/main/java/santech/controller/UserController.java
@@ -1,11 +1,15 @@
 package santech.controller;
 
+import org.primefaces.event.CloseEvent;
+import org.primefaces.event.ToggleEvent;
+import santech.TempBean;
 import santech.model.User;
 import santech.service.SanTechUserService;
 
 import javax.annotation.PostConstruct;
-import javax.ejb.EJB;
 import javax.enterprise.context.SessionScoped;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
 import javax.inject.Inject;
 import javax.inject.Named;
 import java.io.Serializable;
@@ -48,8 +52,16 @@ public class UserController implements Serializable {
 		filterUserList = new ArrayList<>();
 		userList = sanTechUserService.getAllUsers();
 	}
+	@Inject
+	private TempBean tempBean;
 
-	public List<User> getAllUser(){
-		return sanTechUserService.getAllUsers();
+	public void onClose(CloseEvent event) {
+		FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO, "Panel Closed ", "Closed panel id:'" + event.getComponent().getId() + "'");
+		FacesContext.getCurrentInstance().addMessage(null, message);
+	}
+
+	public void onToggle(ToggleEvent event) {
+		FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO, event.getComponent().getId() + " toggled", "  Status:" + event.getVisibility().name() + tempBean.getTestMessage());
+		FacesContext.getCurrentInstance().addMessage(null, message);
 	}
 }
